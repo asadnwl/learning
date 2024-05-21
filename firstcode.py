05d3815fcba444aa741b7331a0182a888ec359d2
asadnwl <chasadnwl@gmail.com>
2024-05-21 14:13:23 +0500

    Add variables

--- firstcode.py
+++ firstcode.py
@@ -1,3 +1,24 @@
 # write first code Hello world!
-
 print("Hello World!")
+
+# Declaring variables
+myNumber = 10       # Int
+print(myNumber)
+myNumber = 10.5    # float
+print(myNumber)
+myNumber = 10j      # complex
+print(myNumber)
+myNumber = "Asad"   # string
+print(myNumber)
+x = True            # boolen
+print(x)
+x = b"Asad"         # binary
+print(x)
+x = ["Muhammad", "Asad", "Muhammad", "Iqran"]       # list
+print(x)
+x = ("Muhammad", "Asad", "Muhammad", "Iqran")       # tuple
+print(x)
+x = {"Muhammad", "Asad", "Muhammad", "Iqran"}       # set
+print(x)
+x = {"name": "Asad", "age" : 25}                    # dictionary
+print(x)
